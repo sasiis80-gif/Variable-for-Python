ee98c80333e00e3f9d7ff286f452735d36b5ee5d
sasiis80-gif <sasini.somaratne80@gmail.com>
2026-03-03 19:48:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#User input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nWelcome to codingal")
